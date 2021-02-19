30140bda66a9d290d25e64a4123ab0e941ed3c4a
Michael O'Connor <michaelp_oconnor@yahoo.ie>
2021-02-19 23:33:48 +0000

    Lab 4 Submission

--- lab-control/Control_Sensors/c_depthSensor.ino
+++ lab-control/Control_Sensors/c_depthSensor.ino
@@ -3,33 +3,49 @@
  * Depth CLASS DEFINITION
  */
 class DepthSensor {
+  
   private:
    
-  byte PIN_Mike;
-  
+  float PINMike;
   unsigned long timeInterval;
 
-  float voltage = PIN_Mike * (5.0/1023.0);
-  float distance = -22*voltage+60; //as per slope staright line graph
-  float distance1 = (0.1064*(pow(voltage,4)))-(7.321*(pow(voltage,3)))+(45.118*(pow(voltage,2)))-(98.843*voltage)+85.045; // This line does the equation of a curve which allows the data to represented more accuratley
-  float x = voltage;
-
+ 
   public:
-    DepthSensor(byte PIN_Mike, unsigned long timeInterval) {
-
-      this->PIN_Mike = PIN_Mike;
-      this->timeInterval = timeInterval;
-      this->x = x;
-      this->distance = distance;
-      this->distance1 = distance1;
-
-      pinMode(PIN_Mike, OUTPUT);
+    
+          float getPINMike(float a){
+            PINMike = analogRead(a);
     }
+      
+      //float voltage = PINMike * (5.0/1023.0);
+  //float distance = -22*voltage+60; //as per slope staright line graph
+ // float distance1 = (0.1064*(pow(voltage,4)))-(7.321*(pow(voltage,3)))+(45.118*(pow(voltage,2)))-(98.843*voltage)+85.045; // This line does the equation of a curve which allows the data to represented more accuratley
+  //float x = voltage;  
+    
+    DepthSensor(float PINMike, unsigned long timeInterval) {
+
+      this->PINMike = PINMike;
+      this->timeInterval = timeInterval;
+ 
+
+
+
+      pinMode(analogRead(PINMike), INPUT);
+    }
+
+
+    float getPINMike(){
+      return analogRead(PINMike);
+    }
+    
+
 
     // Code
     void DepthCheck() {
-     Serial.print("distance = "); Serial.print(distance);Serial.print("   voltage = ");Serial.print(voltage);
-     Serial.println(" ");Serial.print("         distance1 = ");Serial.print(distance1);Serial.print("      ");
-      }
+
+
+    //Serial.print("distance = "); Serial.print(distance);Serial.print("   voltage = ");Serial.print(voltage);
+  // Serial.println(" ");Serial.print("         distance1 = ");Serial.print(distance1);Serial.print("      ");
+    
+     }
     
 };
--- lab-control/Control_Sensors/z_main.ino
+++ lab-control/Control_Sensors/z_main.ino
@@ -1,13 +1,14 @@
 
 #define PIN_LED1 7
-#define PIN_Mike A0
+#define PINMike analogRead(A0)
 
 
 /*
  *  BLINKER CLASS VARIABLES DECLARATION
  */
 Blinker blink1 = Blinker(PIN_LED1, 500, 500);
-DepthSensor Depth = DepthSensor(PIN_Mike,500);
+DepthSensor Depth = DepthSensor(analogRead (PINMike),500);
+
 
 void setup() {
 Serial.begin(9600);
@@ -16,5 +17,28 @@ Serial.begin(9600);
 void loop() {
   blink1.check();
   Depth.DepthCheck();
-  
+
+
+// Serial.println(Depth.getPINMike());                       
+
+
+/* I WAS UNABLE TO GET THE ANALOG READINGS TO BE ANTHING OTHER THAN '0' WITHOUT DOING A GET VALUE IN THE MAIN,
+   AND I THEN HAD TO DO THE CALCULATIONS IN THE MAIN, I AM ABLE TO GET VALUES FROM PRIVATE IT WAS JUST AN ISSUE WITH THE ANALOG VALUE.
+   WHEN I TRIED GETTING IT IN THE PUBLIC CLASS FROM THE PRIVATE I KEPT GETTING 0V, FROM THE ANALOG INPUT ON MY SERIAL MONITOR. 
+   THIS FILE IS A MESS BECAUSE I HAVE ATTEMTED SO MANY DIFFERENT WAYS AND WAS DOING TESTS IN THE MIDDLE.
+
+  I KNOW THE MILLIS FUNCTION IS NEEDED TO STAEDY THE READINGS BUT TRYING TO GET THE OTHER PART EVEN CLOSE TO WORKING HAS EAT INTO MY TIME.
+   */
+
+
+  float voltage = Depth.getPINMike()*(5.0/1023.0);
+  float distance = -22*voltage+60; //as per slope staright line graph
+  float distance1 = (0.1064*(pow(voltage,4)))-(7.321*(pow(voltage,3)))+(45.118*(pow(voltage,2)))-(98.843*voltage)+85.045; // This line does the equation of a curve which allows the data to represented more accuratley
+  float x = voltage;  
+  //Serial.println(a);
+  //Serial.println(Depth.getPINMike());
+ // Serial.println(Depth.getvoltage());
+
+     Serial.print("distance = "); Serial.print(distance);Serial.print("   voltage = ");Serial.print(voltage);
+   Serial.println(" ");Serial.print("         distance1 = ");Serial.print(distance1);Serial.print("      ");
 }
